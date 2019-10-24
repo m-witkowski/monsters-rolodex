7050e77dd6a8d1e024fc54f62d74ff44b96b334e
Michał Witkowski <mwitkowski97@icloud.com>
2019-10-24 10:03:55 +0200

    adding files for github pages

---
+++ src/components/card_list/card_list.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from '../card/card.component';
+import './card_list.css'
+
+export const CardList = props =>
+    <div className='card_list'>
+        {props.monsters.map(monster =>
+            <Card key={monster.id} monster={monster}/>)}
+    </div>;
+;

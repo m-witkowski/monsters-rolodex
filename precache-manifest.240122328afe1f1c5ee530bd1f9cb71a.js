428cad81d5c32ee5f2eb1155e9a9c61440fa6280
Michał Witkowski <mwitkowski97@icloud.com>
2019-10-24 10:03:22 +0200

    Updates

---
+++ precache-manifest.240122328afe1f1c5ee530bd1f9cb71a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7361cf113c67b0cea0e42136aae5f4bd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "82ecbe301612152e2d17",
+    "url": "/monsters-rolodex/static/css/main.9723baf1.chunk.css"
+  },
+  {
+    "revision": "a7ffd671cb21d8ff7fb4",
+    "url": "/monsters-rolodex/static/js/2.fe5198a4.chunk.js"
+  },
+  {
+    "revision": "82ecbe301612152e2d17",
+    "url": "/monsters-rolodex/static/js/main.f3b7b338.chunk.js"
+  },
+  {
+    "revision": "d78c7e54dca26e3c25e2",
+    "url": "/monsters-rolodex/static/js/runtime-main.aaf9d0bd.js"
+  }
+]);
